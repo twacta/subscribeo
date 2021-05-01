23cc6d2b65102f99c00f9bfad1cba367d723dbd6
Fabien RIES <48822500+twacta@users.noreply.github.com>
2021-05-01 15:43:07 +0200

    Updates

---
+++ component---src-pages-404-js-479a3fb822ba0028dec3.js
@@ -0,0 +1,2 @@
+(self.webpackChunkgatsby_starter_lander=self.webpackChunkgatsby_starter_lander||[]).push([[883],{9616:function(t,e,n){"use strict";n.r(e);n(7294);var u=n(2813),a=n(8100);e.default=function(){return(0,a.tZ)(u.Z,null,(0,a.tZ)("h1",null,"Not Found"),(0,a.tZ)("p",null,"You just hit a page that doesn't exist..."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-479a3fb822ba0028dec3.js.map
